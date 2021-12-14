34ca959be31f0dd279dff3e830b3b03c6f49ef7c
Yelena <54732952+Yelena1212@users.noreply.github.com>
2021-12-14 11:57:20 -0800

    Add files via upload

---
+++ Bara4113/site_folser/HTML/app.js
@@ -0,0 +1,13 @@
+$(function () {
+    $("[data-modal]").on("click", function (event) {
+        event.preventDefault();
+
+        $(modal_resume).addClass('show');
+    })
+
+    $(modal_resume).on("click", function () {
+        $(modal_resume).removeClass('show');
+    })
+
+
+});
